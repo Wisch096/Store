2fe805372fe8102cfe873bfa3d9f9d65704cb06c
Matheus Wisch <wisch.pg@outlook.com>
2025-03-09 20:27:17 -0300

    :sparkles: Feat: Create discount entity

---
+++ Store.Domain/Entities/Discount.cs
@@ -0,0 +1,23 @@
+﻿namespace Store.Domain.Entities;
+
+public class Discount : Entity
+{
+    public Discount(decimal amout, DateTime expireDate)
+    {
+        Amout = amout;
+        ExpireDate = expireDate;
+    }
+
+    public decimal Amout { get; set; }
+    public DateTime ExpireDate { get; set; }
+
+    public bool IsValid()
+    {
+        return DateTime.Compare(DateTime.Now, ExpireDate) < 0;
+    }
+
+    public decimal Value()
+    {
+        return IsValid() ? Amout : 0;
+    }
+}
