5069d34df0cbdce4bcbe8cf0042e01b394b11732
Matheus Wisch <wisch.pg@outlook.com>
2025-03-10 18:23:15 -0300

    :sparkles: Feat: Validating OrderItem

--- Store.Domain/Entities/OrderItem.cs
+++ Store.Domain/Entities/OrderItem.cs
@@ -1,9 +1,18 @@
-﻿namespace Store.Domain.Entities;
+﻿using Flunt.Validations;
+
+namespace Store.Domain.Entities;
 
 public class OrderItem : Entity
 {
     public OrderItem(Product product, int quantity)
     {
+        AddNotifications(
+            new Contract<Order>()
+                .Requires()
+                .IsNotNull(product, "Product", "Product is required")
+                .IsGreaterThan(quantity, 0, "Quantity", "Quantity must be greater than 0")
+        );
+        
         Product = product;
         Price = Product != null ? Product.Price : 0;
         Quantity = quantity;
