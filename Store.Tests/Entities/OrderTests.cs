80616fe7a80f060019e19025574872e933b3ab58
Matheus Wisch <wisch.pg@outlook.com>
2025-03-10 18:39:49 -0300

    🧪Test: Testing order payment

--- Store.Tests/Entities/OrderTests.cs
+++ Store.Tests/Entities/OrderTests.cs
@@ -25,4 +25,14 @@ public class OrderTests
        var order = new Order(_customer, 0, null);
        Assert.AreEqual(order.Status, EOrderStatus.WaitingPayment);
     }
+    
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Dado_um_pagamento_do_pedido_seu_status_deve_ser_aguardando_entrega()
+    {
+        var order = new Order(_customer, 0, null);
+        order.AddItem(_product, 1);
+        order.Pay(10);
+        Assert.AreEqual(order.Status, EOrderStatus.WaitingDelivery);
+    }
 }
