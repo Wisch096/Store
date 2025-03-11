2f795e2a3aeb649e9d453bf9e098743c93062ec2
Matheus Wisch <wisch.pg@outlook.com>
2025-03-11 16:30:52 -0300

    :sparkles: Feat: Create some commands and adding fail fast validating

---
+++ Store.Domain/Commands/CreateOrderCommand.cs
@@ -0,0 +1,32 @@
+﻿using Flunt.Notifications;
+using Store.Domain.Commands.Interfaces;
+
+namespace Store.Domain.Commands;
+
+public class CreateOrderCommand : Notifiable<Notification>, ICommand
+{
+    public CreateOrderCommand(string customer, string zipCode, string promoCode, IList<CreateOrderItemCommand> items)
+    {
+        Customer = customer;
+        ZipCode = zipCode;
+        PromoCode = promoCode;
+        Items = items;
+    }
+
+    public CreateOrderCommand()
+    {
+        Items = new List<CreateOrderItemCommand>();
+    }
+
+    public string Customer { get; set; }
+
+    public string ZipCode { get; set; }
+
+    public string PromoCode { get; set; }
+    public IList<CreateOrderItemCommand> Items { get; set; }
+    
+    public void Validate()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Store.Domain/Commands/CreateOrderItemCommand.cs
@@ -0,0 +1,27 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace Store.Domain.Commands.Interfaces;
+
+public class CreateOrderItemCommand : Notifiable<Notification>, ICommand
+{
+    public CreateOrderItemCommand(Guid product, int quantity)
+    {
+        Product = product;
+        Quantity = quantity;
+    }
+
+    public CreateOrderItemCommand() { }
+
+    public Guid Product { get; set; }
+    public int Quantity { get; set; }
+    
+    public void Validate()
+    {
+        AddNotifications(new Contract<CreateOrderItemCommand>()
+            .Requires()
+            .IsGreaterThan(Quantity, 0, "Quantity", "Quantity must be greater than 0")
+            .AreEquals(Product.ToString(), 32, "Product", "Product must be 32 characters long")
+        );
+    }
+}
