9237bdd971bbd16079f56a5f619c8b5ef26bf5b4
Matheus Wisch <wisch.pg@outlook.com>
2025-03-11 13:51:06 -0300

    🧪 Test: Testing invalid item

--- Store.Tests/obj/Debug/net8.0/Store.Tests.AssemblyInfo.cs
+++ Store.Tests/obj/Debug/net8.0/Store.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+521f4958013370339fbf063d36c00f6e1b383605")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc52cb6914ac970e5ff1d002ebb74eccc77b8542")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
