521f4958013370339fbf063d36c00f6e1b383605
Matheus Wisch <wisch.pg@outlook.com>
2025-03-11 13:44:50 -0300

    🧪 Test: Testing cancel order

--- Store.Tests/obj/Debug/net8.0/Store.Tests.AssemblyInfo.cs
+++ Store.Tests/obj/Debug/net8.0/Store.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa9d26b3cecd72945ee38c467a43a00ddead3434")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80616fe7a80f060019e19025574872e933b3ab58")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
