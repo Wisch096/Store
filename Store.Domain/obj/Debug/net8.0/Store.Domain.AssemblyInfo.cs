04fdb6f72f5a89c5ac19f053ffe2758e2f1189ef
Matheus Wisch <wisch.pg@outlook.com>
2025-03-26 18:09:34 -0300

    dev

--- Store.Domain/obj/Debug/net8.0/Store.Domain.AssemblyInfo.cs
+++ Store.Domain/obj/Debug/net8.0/Store.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e5af4131551bfaabc7da5256f245955a35e93d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c6a1f6384ef80875d0b964c0b3d8c1c502f6418")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
